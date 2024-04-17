9b51f2aed254be3e8147e55f12e13024027e591f
Rishab <rishab28guleria@gmail.com>
2024-04-17 19:36:00 +0530

    Added blobs to HomeSectionTwo

--- frontend/src/components/HomeSectionTwo.js
+++ frontend/src/components/HomeSectionTwo.js
@@ -1,3 +1,4 @@
+import blob from "../Blob.png";
 export default function HomeSectionTwo() {
   return (
     <div>
@@ -10,36 +11,66 @@ export default function HomeSectionTwo() {
           technology on StoryForge.
         </p>
       </div>
-      <div className="flex flex-col lg:flex-row py-5 px-5 items-center gap-8 lg:justify-center lg:items-start">
-        <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
-          <div className="text-8xl font-extrabold">1</div>
-          <div className="flex flex-col gap-5">
-            <h2 className="text-2xl font-semibold">Create</h2>
-            <p className="w-56">
-              Share your original story on StoryForge. Find the writing
-              resources you need to craft a story only you can tell.
-            </p>
+      <div className="flex flex-col xl:flex-row py-5 px-5 items-center gap-4 xl:justify-center xl:items-start">
+        <div className="flex flex-row xl:flex-col gap-5">
+          <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
+            <div className="text-8xl font-extrabold">1</div>
+            <div className="flex flex-col gap-5">
+              <h2 className="text-2xl font-semibold">Create</h2>
+              <p className="w-56 h-32">
+                Share your original story on StoryForge. Find the writing
+                resources you need to craft a story only you can tell.
+              </p>
+            </div>
+          </div>
+          <div className="hidden sm:flex justify-center relative">
+            <img src={blob} className="w-80 relative" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+              <p className="text-white text-2xl font-bold w-24 text-center">
+                50+ Writing Resources
+              </p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
-          <div className="text-8xl font-extrabold">2</div>
-          <div className="flex flex-col gap-5">
-            <h2 className="text-2xl font-semibold">Build</h2>
-            <p className="w-56">
-              Establish a global fan base as your story gains readership and
-              momentum. Connect with other like-minded writers through
-              storytelling.
-            </p>
+        <div className="flex flex-row xl:flex-col gap-5">
+          <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
+            <div className="text-8xl font-extrabold">2</div>
+            <div className="flex flex-col gap-5">
+              <h2 className="text-2xl font-semibold">Build</h2>
+              <p className="w-56 h-32">
+                Establish a global fan base as your story gains readership and
+                momentum. Connect with other like-minded writers through
+                storytelling.
+              </p>
+            </div>
+          </div>
+          <div className="hidden sm:flex justify-center relative">
+            <img src={blob} className="w-80 relative" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+              <p className="text-white text-2xl font-bold w-24 text-center">
+                90 Million+ People
+              </p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
-          <div className="text-8xl font-extrabold">3</div>
-          <div className="flex flex-col gap-5">
-            <h2 className="text-2xl font-semibold">Amplify</h2>
-            <p className="w-56">
-              Gain StoryForge Star status and get your story published or
-              adapted into film or television with StoryForge Studios!
-            </p>
+        <div className="flex flex-row xl:flex-col gap-5">
+          <div className="flex flex-row gap-10 w-80 transition duration-200 ease-in-out transform hover:scale-105 lg:w-96">
+            <div className="text-8xl font-extrabold">3</div>
+            <div className="flex flex-col gap-5">
+              <h2 className="text-2xl font-semibold">Amplify</h2>
+              <p className="w-56 h-32">
+                Gain Wattpad Star status and get your story published or adapted
+                into film or television with Wattpad WEBTOON Studios!
+              </p>
+            </div>
+          </div>
+          <div className="hidden sm:flex justify-center relative">
+            <img src={blob} className="w-80 relative" />
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+              <p className="text-white text-2xl font-bold w-24 text-center">
+                1000+ Story Deals
+              </p>
+            </div>
           </div>
         </div>
       </div>
