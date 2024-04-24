12001d0885b65ef6da707ddc94687c60b4de31eb
Rishab <rishab28guleria@gmail.com>
2024-04-24 13:17:29 +0530

    Created LoginBackground component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import Home from "./components/Home";
-
+import Login from "./components/Login.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route exact path="/login" element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginBackground from './LoginBackground';
+export default function Login() {
+  return (
+      <div className='h-screen w-screen flex items-center justify-center'>
+        <LoginBackground/>
+      </div>
+  )
+}
