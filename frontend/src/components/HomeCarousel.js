66b17c9d80dd2b963abdf3b462f832eabdae1e81
Rishab <rishab28guleria@gmail.com>
2024-05-04 00:38:18 +0530

    Made the curves responsive

--- frontend/src/components/HomeCarousel.js
+++ frontend/src/components/HomeCarousel.js
@@ -12,9 +12,9 @@ export default function CarouselTransition() {
         backgroundSize: "cover",
         position: "relative",
       }}
-      className="justify-center items-center flex pt-24"
+      className="justify-center items-center flex pt-32"
     >
-      <div class="custom-shape-divider-top-1714759530">
+      <div className="custom-shape-divider-top-1714759530">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +23,7 @@ export default function CarouselTransition() {
         >
           <path
             d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"
-            class="shape-fill"
+            className="shape-fill"
           ></path>
         </svg>
       </div>
