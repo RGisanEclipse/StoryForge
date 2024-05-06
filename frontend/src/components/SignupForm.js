2ec40d6f907ff0d070725f06309897f84e852654
Rishab <rishab28guleria@gmail.com>
2024-05-06 23:31:22 +0530

    UI Improvements.

--- frontend/src/components/SignupForm.js
+++ frontend/src/components/SignupForm.js
@@ -190,19 +190,19 @@ export default function SignupForm() {
                 <div className="flex flex-row justify-between gap-10">
                   <div>
                     <GoogleIcon
-                      className="text-gray-500 cursor-pointer"
+                      className="text-gray-400 cursor-pointer"
                       style={{ fontSize: "50px" }}
                     />
                   </div>
                   <div>
                     <FacebookSharpIcon
-                      className="text-gray-500 cursor-pointer"
+                      className="text-gray-400 cursor-pointer"
                       style={{ fontSize: "50px" }}
                     />
                   </div>
                   <div>
                     <TwitterIcon
-                      className="text-gray-500 cursor-pointer"
+                      className="text-gray-400 cursor-pointer"
                       style={{ fontSize: "50px" }}
                     />
                   </div>
