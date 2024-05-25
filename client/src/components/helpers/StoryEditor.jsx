8c72ab5737b8b4d91382038e74d983f608b9a251
Rishab <rishab28guleria@gmail.com>
2024-05-25 23:28:40 +0530

    Changes in the StoryEditor Component

--- client/src/components/helpers/StoryEditor.jsx
+++ client/src/components/helpers/StoryEditor.jsx
@@ -7,6 +7,7 @@ import ListIcon from "@mui/icons-material/List";
 import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 import { Button } from "@mui/material";
 import "draft-js/dist/Draft.css";
+
 function StoryEditor() {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
@@ -23,9 +24,11 @@ function StoryEditor() {
   const handleItalicClick = () => {
     onChange(RichUtils.toggleInlineStyle(editorState, "ITALIC"));
   };
+
   const handleUnderlineClick = () => {
     onChange(RichUtils.toggleInlineStyle(editorState, "UNDERLINE"));
   };
+
   const handleUnorderedListClick = () => {
     onChange(RichUtils.toggleBlockType(editorState, "unordered-list-item"));
   };
@@ -33,42 +36,24 @@ function StoryEditor() {
   const handleOrderedListClick = () => {
     onChange(RichUtils.toggleBlockType(editorState, "ordered-list-item"));
   };
+
   return (
-    <div
-      className="h-full w-full rounded text-white"
-      style={{ scrollbarWidth: "none" }}
-    >
+    <div className="h-full w-full py-4 text-white">
       <h1 className="text-sm font-medium text-white">Content</h1>
       <div className="mt-2 flex justify-between items-center">
         <div>
-          <FormatBoldIcon
-            onClick={handleBoldClick}
-            className="cursor-pointer"
-          />
-          <FormatItalicIcon
-            onClick={handleItalicClick}
-            className="cursor-pointer ml-2"
-          />
-          <FormatUnderlinedIcon
-            onClick={handleUnderlineClick}
-            className="cursor-pointer ml-2"
-          />
-          <ListIcon
-            onClick={handleUnorderedListClick}
-            className="cursor-pointer ml-2"
-          />
-          <FormatListNumberedIcon
-            onClick={handleOrderedListClick}
-            className="cursor-pointer ml-2"
-          />
+          <FormatBoldIcon onClick={handleBoldClick} className="cursor-pointer" />
+          <FormatItalicIcon onClick={handleItalicClick} className="cursor-pointer ml-2" />
+          <FormatUnderlinedIcon onClick={handleUnderlineClick} className="cursor-pointer ml-2" />
+          <ListIcon onClick={handleUnorderedListClick} className="cursor-pointer ml-2" />
+          <FormatListNumberedIcon onClick={handleOrderedListClick} className="cursor-pointer ml-2" />
         </div>
         <Button sx={{ color: "white", textTransform: "initial" }}>Save & Post</Button>
       </div>
-      <div
-        className=" mt-2 border h-5/6 p-2 rounded-lg overflow-y-auto"
-        style={{ scrollbarWidth: "none" }}
-      >
-        <Editor editorState={editorState} onChange={onChange} />
+      <div className="mt-2 border h-5/6 p-2 rounded-lg">
+        <div className="h-full">
+          <Editor editorState={editorState} onChange={onChange} />
+        </div>
       </div>
     </div>
   );
